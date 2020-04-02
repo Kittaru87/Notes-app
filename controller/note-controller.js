e2129f60f5412696b85b3999aae544c1e7874a83
Sophia Bell <sophbell87@gmail.com>
2020-04-02 13:39:29 +0100

    removing text box when note app in single view

--- controller/note-controller.js
+++ controller/note-controller.js
@@ -11,17 +11,25 @@
      noteDisplay.innerHTML = this.notelistview.noteView();
   };
 
+  NoteController.prototype.hideCreateNote = function() {
+    window.addEventListener('hashchange', function(){
+      document.getElementById('text').style.display = "none";
+    });  
+  };
+
   NoteController.prototype.showAllOfSingleNote = function() {
     var list = this.notelist
     window.addEventListener('hashchange', function(){
       var noteId = window.location.hash.split('#note_')[1];
       document.getElementById('app').innerHTML = list.getNoteByID([noteId]);
+      hideCreateNote();
     }, false);
   };
 
   NoteController.prototype.createNewNote = function () {
-    document.getElementById("submit").addEventListener("click", function(event){
+    document.getElementById('text').addEventListener('submit', function(event){
       event.preventDefault()
+      console.log("event")
     });
   };
 
@@ -29,6 +37,7 @@
   exports.NoteController.insertNote = NoteController.insertNote;
   exports.NoteController.showAllOfSingleNote = NoteController.showAllOfSingleNote;
   exports.NoteController.createNewNote = NoteController.createNewNote;
+  exports.NoteController.hideCreateNote = NoteController.hideCreateNote;
 
 })(this);
 
@@ -46,3 +55,4 @@ var n = new NoteList();
  notecontrollerexample.showList();
  notecontrollerexample.showAllOfSingleNote();
  notecontrollerexample.createNewNote();
+ notecontrollerexample.hideCreateNote();
